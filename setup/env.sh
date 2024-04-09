317aaad79061ec0ec8154545888185796005b2bf
imssyang <imssyang@gmail.com>
2024-04-09 17:02:14 +0800

    update env.sh

--- setup/env.sh
+++ setup/env.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-export QT5_DISABLE_ENV=yes
 export QT_PLUGIN_PATH=/opt/qt5/plugins
-eval "optbin -s /opt/qt5/bin"
-eval "optlib -s /opt/qt5/lib"
-eval "optpkg -s /opt/qt5/lib/pkgconfig"
